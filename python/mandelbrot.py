cb50340d73fac8f921fac603749721855a35bc7e
2Zhur <mkzhurav@gmail.com>
2023-02-04 19:56:35 -0500

    saving progress

--- python/mandelbrot.py
+++ python/mandelbrot.py
@@ -2,31 +2,36 @@ from PIL import Image
 import numpy as np
 
 
-def mandelbrot_core_fn(z: complex, c: complex):
+def mandelbrot_core_fn(z, c):
     return z * z + c
 
 
-def get_pixel_brightness(c: complex, max_iterations: int) -> int:
-    count = 0
-    step_val = mandelbrot_core_fn(0, c)
-    while (abs(step_val) > 1e-8) and (abs(step_val) < 2) and (count < max_iterations):
-        step_val = mandelbrot_core_fn(step_val, c)
-        count += 1
-    if max_iterations >= count:
-        return 255 - int(float(count * 255) / float(max_iterations))
-    else:
-        return 255
+def get_pixel_values(z_mtrx):
+    
+    return
+
 
 
 if __name__ == "__main__":
-    square_img_size = 1024
+    img_size = 1024
+    img_shape = (img_size, img_size)
     c_matrix = np.array(
         [
-            complex(real, imag)
-            for real in range(square_img_size)
-            for imag in range(square_img_size)
+            complex(real, imag) / (2 * img_size)
+            for real in range(-1 * (img_size / 2), (img_size / 2))
+            for imag in range(-1 * (img_size / 2), (img_size / 2))
         ]
-    ).reshape((square_img_size, square_img_size))
+    ).reshape(img_shape)
+    z_matrix = np.zeros(img_shape)
+    num_iterations = 10
+    for i in range(num_iterations):
+        z_matrix = mandelbrot_core_fn(z_matrix, c_matrix)
+    bool_mtrx = np.isfinite(z_matrix)
+
+    img_array_shape = (*z_matrix.shape, 3)
+    max_val = np.nanmax(z_matrix)
+    z_matrix_normalized = z_matrix / max
+
     w, h = 1024, 1024
     max_iterations = 300
     t = (w, h, 3)
@@ -34,7 +39,7 @@ if __name__ == "__main__":
     for i in range(-512, 512):
         for j in range(-512, 512):
             c = complex(float(i) / 256, float(j) / 256)
-            tupl = tuple([get_pixel_brightness(c, max_iterations) for _ in range(3)])
+            tupl = tuple([get_pixel_values(c, max_iterations) for _ in range(3)])
             A[i + 512, j + 512] = tupl
             # A[i + 512, j + 512] = (255, 0, 0)
     img = Image.fromarray(A, "RGB")
